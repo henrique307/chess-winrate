fdab02fab1f89f9eea96b0c66827c8b2bb1efa04
henrique <henrique.florencio307@gmail.com>
2022-09-30 19:21:19 -0300

    começando meu primeiro projeto em Angular =)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { ListaComponent } from './lista/lista.component';
+import { HeaderComponent } from './header/header.component';
+import { InfoComponent } from './perfil/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerfilComponent,
+    PesquisaComponent,
+    ListaComponent,
+    HeaderComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
